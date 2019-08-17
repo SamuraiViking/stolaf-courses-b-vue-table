2742a2b741aea24f1c70f1771b2283db802cde81
Kevin <nelson67@stolaf.edu>
2019-08-17 13:40:30 -0500

    adds dropdown selector in columns

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,12 @@
   </div>
 </template>
 
-<style>
+<style meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+.selector-for-some-widget {
+  box-sizing: content-box;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,38 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <b-table striped hover :items="items">
+      <template slot="top-row" slot-scope="{ fields }">
+        <td v-for="field in fields" :key="field.keys">
+          <form>
+            <select>
+              <option>A</option>
+              <option>B</option>
+              <option>C</option>
+              <option>D</option>
+              <option>E</option>
+            </select>
+          </form>
+        </td>
+<!--         <td v-for="field in fields" :key="field.key">
+          <input v-model="filters[field.key]" :placeholder="field.label">
+        </td> -->
+      </template>
+    </b-table>
+
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  export default {
+    data() {
+      return {
+        items: [
+          { age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
+          { age: 21, first_name: 'Larsen', last_name: 'Shaw' },
+          { age: 89, first_name: 'Geneva', last_name: 'Wilson' },
+          { age: 38, first_name: 'Jami', last_name: 'Carney' }
+        ]
+      }
+    }
   }
-}
 </script>
